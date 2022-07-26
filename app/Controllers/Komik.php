efeb4567c02a6313753631f9ff1e3e6e015ccb58
mdwisu <dwisusanto784@gmail.com>
2022-07-26 17:38:07 +0700

    ci4 app wpu

---
+++ app/Controllers/Komik.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\KomikModel;
+
+class Komik extends BaseController
+{
+    protected $komikModel;
+    public function __construct() {
+        $this->komikModel = new KomikModel();
+    }
+
+    public function index()
+    {
+        // $komik = $this->komikModel->findAll();
+
+        $data = [
+            'title' => 'Daftar Komik | Web Dwi',
+            'komik' => $this->komikModel->getKomik(),
+        ];
+        return view('komik/index', $data);
+    }
+
+    public function detail($slug)
+    {
+        $data = [
+            'title' => 'Detail Komik',
+            'komik'=>  $this->komikModel->getKomik($slug),
+        ];
+
+        if (empty($data['komik'])) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Judul komik '. $slug . ' tidak ditemukan');
+            
+        }
+        return view('komik/detail', $data);
+    }
+
+    public function create()
+    {
+        $data = [
+            'title' => 'Form Tambah Data Komik',
+            'validation' => \Config\Services::validation()
+        ];
+        return view('komik/create', $data);
+    }
+
+
+    public function save()
+    {
+        // validasi input
+        if (!$this->validate([
+            'judul' => [
+                'rules' => 'required|is_unique[komik.judul]',
+                'errors' => [
+                    'required' => '{field} komik harus diisi',
+                    'is_unique' => '{field} komik sudah terdaftar'
+                ],
+            ],
+        ])) {
+            $validation = \Config\Services::validation();
+            return redirect()->to('komik/create')->withInput()->with('validation', $validation);
+        }
+        $slug = url_title($this->request->getVar('judul'), '-', true);
+        $this->komikModel->save([
+            'judul' => $this->request->getVar('judul'),
+            'slug' => $slug,
+            'penulis' => $this->request->getVar('penulis'),
+            'penerbit' => $this->request->getVar('penerbit'),
+            'sampul' => $this->request->getVar('sampul')
+        ]);
+
+        session()->setFlashdata('pesan', 'Data Berhasil ditambahkan');
+        return redirect()->to('/komik');
+    }
+
+    public function delete($id)
+    {
+        $this->komikModel->delete($id);
+        session()->setFlashdata('pesan', 'Data Berhasil dihapus');
+        return redirect()->to('/komik');
+    }
+
+    public function edit($slug)
+    {
+        $data = [
+            'title' => 'Form Ubah Data Komik',
+            'validation' => \Config\Services::validation(),
+            'komik' => $this->komikModel->getKomik($slug),
+        ];
+        return view('komik/edit', $data);
+    }
+}
---
+++ app/Controllers/Pages.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Home | Web Dwi'
+        ];
+        return view('pages/home', $data);
+    }
+    public function about()
+    {
+        $data = [
+            'title' => 'about | Web Dwi'
+        ];
+        return view('pages/about', $data);
+    }
+    public function contact()
+    {
+        $data = [
+            'title' => 'contact | Web Dwi',
+            'alamat' => [
+                [
+                    'tipe' => 'rumah',
+                    'alamat' => 'Jl. abc No. 123',
+                    'kota' => 'bandung'
+                ],
+                [
+                    'tipe' => 'kantor',
+                    'alamat' => 'Jl. cba No. 321',
+                    'kota' => 'bogor'
+                ],
+            ],
+        ];
+        return view('pages/contact', $data);
+    }
+
+}
